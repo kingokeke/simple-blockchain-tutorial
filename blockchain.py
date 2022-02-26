34214ba1c33bc552df01067281edc629a3fb9256
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-26 08:37:18 +0100

    Add add_block method

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,11 @@
 from hashlib import sha256
+import json
+import time
+
 
 class Chain:
     def __init__(self) -> None:
-        self.blochchain = []
+        self.blockchain = []
         self.pending = []
         self.add_block(prevhash="Genesis", proof=123)
 
@@ -13,4 +16,15 @@ class Chain:
     def compute_hash(self, block):
         json_block = json.dumps(block, sort_keys=True).encode()
         return sha256(json_block).hexdigest()
+
+    def add_block(self, proof, prevhash=None):
+        block = {
+            "index": len(self.blockchain),
+            "timestamp": time.time(),
+            "transactions": self.pending,
+            "proof": proof,
+            "prevhash": prevhash or self.compute_hash(self.blockchain[-1]),
+        }
+        self.pending = []
+        self.blockchain.append(block)
 
