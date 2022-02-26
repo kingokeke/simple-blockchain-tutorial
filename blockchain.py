61c3ede5e8dac12ffc8e9f92e57a3ef21dd8089c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-26 08:37:35 +0100

    Add data to test blockchain out

--- blockchain.py
+++ blockchain.py
@@ -28,3 +28,17 @@ class Chain:
         self.pending = []
         self.blockchain.append(block)
 
+
+chain = Chain()
+
+t1 = chain.add_transaction("Vitalik", "Satoshi", 100)
+t2 = chain.add_transaction("Satoshi", "Alice", 10)
+t3 = chain.add_transaction("Alice", "Charlie", 34)
+chain.add_block(12345)
+
+t4 = chain.add_transaction("Bob", "Eve", 23)
+t5 = chain.add_transaction("Dennis", "Brian", 3)
+t6 = chain.add_transaction("Ken", "Doug", 88)
+chain.add_block(6789)
+
+print(chain.blockchain)
