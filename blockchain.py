1e63d4e80d2dbd99f86ae8c4c931e34302b540ed
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-26 08:14:54 +0100

    Add add_transaction method

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,11 @@
 from hashlib import sha256
 
 class Chain:
-  def __init__(self) -> None:
-      self.blochchain = []
-      self.pending = []
-      self.add_block(prevhash="Genesis", proof=123)
+    def __init__(self) -> None:
+        self.blochchain = []
+        self.pending = []
+        self.add_block(prevhash="Genesis", proof=123)
+
+    def add_transaction(self, sender, recipient, amount):
+        transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+        self.pending.append(transaction)
